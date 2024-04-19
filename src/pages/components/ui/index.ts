a3fe1f9510004c5ea564039d3536644589fa3156
ngch43 <neil.chacon@mediaadgo.com>
2024-04-19 23:26:27 +0200

    Create a practice course to learn how to use React and Tailwind

---
+++ src/pages/components/ui/index.ts
@@ -0,0 +1,5 @@
+import Input from "@/pages/components/ui/Input";
+import Label from "@/pages/components/ui/Label";
+import Button from "@/pages/components/ui/Button";
+
+export { Button, Label, Input };
